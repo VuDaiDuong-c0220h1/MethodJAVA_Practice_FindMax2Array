fdd22d1923970672b4ce09de1185b19d6fe9ff38
VuDaiDuong-c0220h1 <61004399+VuDaiDuong-c0220h1@users.noreply.github.com>
2020-03-23 23:06:22 +0700

    Add files via upload

---
+++ src/Element.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kích thước phần tử mảng thứ nhất: ");
+        int firstSize = sc.nextInt();
+        System.out.println();
+        System.out.println("Kích thước phần tử mảng thứ hai: ");
+        int secondSize = sc.nextInt();
+        int [][]array = new int[firstSize][secondSize];
+        for (int i = 0; i < array.length; i++){
+            for(int j= 0; j < array[i].length; j++) {
+                System.out.println("Nhập phần tử thứ " + (j+1) + " của mảng thứ " + (i+1));
+                array[i][j] = sc.nextInt();
+            }
+        }
+        int max = array[0][0];
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (max < array[i][j]) {
+                    max = array[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        System.out.println("Phần tử lớn nhất là: "
+                + max + " ở vị trí hàng " + (x+1) + " cột " + (y+1));
+    }
+}
